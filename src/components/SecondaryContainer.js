44518b320e1c20892d8c2beb44e00d27945aa590
Hrithik Gupta <hrithikguptagithub@gmail.com>
2024-04-29 01:53:20 +0530

    Update SecondaryContainer.js

--- src/components/SecondaryContainer.js
+++ src/components/SecondaryContainer.js
@@ -9,7 +9,7 @@ const SecondaryContainer = () => {
   return (
     movies && (
       <div className="bg-black">
-        <div className="-mt-52 relative z-20">
+        <div className="-mt-40 relative z-20">
           <MovieList title={"Now Playing"} movies={movies.nowPlayingMovies} />
           <MovieList title={"Popular"} movies={movies.popularMovies} />
           <MovieList title={"Top Rated"} movies={movies.topRatedMovies} />
